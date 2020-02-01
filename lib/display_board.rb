f1ee5758d40c3c11ba4eddd9650a77808c5e0eb9
Trevor Jones <tjones009@yahoo.com>
2020-02-01 20:28:13 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,29 +1,29 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-the_board = [" "," "," "," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
 
 def display_board(the_board)
- puts "#{the_board[0]} | #{the_board[1]} | #{the_board[2]}"
+ puts "#{board[0]} | #{board[1]} | #{board[2]}"
  puts "-----------"
- puts "#{the_board[3]} | #{the_board[4]} | #{the_board[5]}"
+ puts "#{board[3]} | #{board[4]} | #{board[5]}"
  puts "-----------"
- puts "#{the_board[6]} | #{the_board[7]} | #{the_board[8]}"
+ puts "#{board[6]} | #{board[7]} | #{board[8]}"
 end
 
-display_board(the_board)
-the_board = [" "," "," "," ","X"," "," "," "," "]
+display_board(board)
+board = [" "," "," "," ","X"," "," "," "," "]
 
-display_board(the_board)
-the_board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+display_board(board)
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
 
 
-display_board(the_board)
-the_board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+display_board(board)
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 
-display_board(the_board)
-the_board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+display_board(board)
+board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
 
-display_board(the_board)
-the_board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+display_board(board)
+board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
 
-display_board(the_board)
+display_board(board)
