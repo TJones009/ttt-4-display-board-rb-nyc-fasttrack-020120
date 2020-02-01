0b6ca7b9c0ed7cd7517ee5b50a6e08c42d25f090
Trevor Jones <tjones009@yahoo.com>
2020-02-01 20:32:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,19 +11,19 @@ def display_board(board)
 end
 
 display_board(board)
-board = [" "," "," "," ","X"," "," "," "," "]
+board = [ " "," "," "," ","X"," "," "," "," "]
 
 display_board(board)
-board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board = [ "X", "X", "X", " ", " ", " ", " ", " ", " " ]
 
 
 display_board(board)
-board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+board = [ "O", "X", " ", " ", "X", " ", "X", "O", " " ]
 
 display_board(board)
-board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+board = [ "O", "O", "O", "O", "O", "O", "O", "O", "O" ]
 
 display_board(board)
-board = ["O", "X", " ", " ", "X", " ", "X", "O", " "]
+board = [ "O", "X", " ", " ", "X", " ", "X", "O", " " ]
 
 display_board(board)
